9555fd8bc09aefb99c2e60a4212ffe6f5f1ae4bc
TibiStayAtHome <62899422+TibiStayAtHome@users.noreply.github.com>
2020-04-14 14:14:27 +0200

    Create scripts.js

---
+++ My Firs WebApp/scripts.js
@@ -0,0 +1,43 @@
+let photo1 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img1.jpg',
+    title: 'Winter',
+    description: 'Winter is the coldest season of the year in polar and temperate zones.'
+};
+let photo2 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img2.jpg',
+    title: 'Wave',
+    description: 'Waves can only exist in fields when there is a force that tends to restore the field to equilibrium'
+};
+let photo3 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img3.jpg',
+    title: 'Forest',
+    description: 'In the forest I breathe in every way that it is possible to expand: in lungs, in brain in soul.'
+};
+let photo4 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img4.jpg',
+    title: 'Sunshine',
+    description: 'Sunshine is the light that comes from the sun. On a clear, cloudless spring day, you can go outside and enjoy the sunshine.'
+};
+let photo5 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img5.jpg',
+    title: 'Trees',
+    description: 'In botany, a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species.'
+};
+let photo6 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img6.jpg',
+    title: 'Autumn',
+    description: 'The autumn was the time of hugs, of evenings with warm drinks and warmer smiles.'
+};
+let photo7 = {
+    photo: '/GreenFoxGithub/PicturesForMyFirstApp/img7.jpg',
+    title: 'Road',
+    description: 'The road was a smooth black river, the sort wheels float so effortlessly along.'
+};
+
+
+
+let imagesData = [photo1, photo2, photo3, photo4, photo5, photo6, photo7];
+let currentPhoto = 0;
+let loadPhoto = (photoNumber) => {
+$('#photo').attr('src', imagesData[currentPhoto].photo)
+};
